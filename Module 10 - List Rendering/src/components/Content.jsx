d88d767e342b4b6e325b2e98a8500aa0f4826b21
CodeWithDeveeswar <k.deveeswar@gmail.com>
2026-09-18 17:38:12 +0530

    Module 10 and 11 added

---
+++ Module 10 - List Rendering/src/components/Content.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { FaTrashCan } from "react-icons/fa6";
+
+const Content = () => {
+  let [items, setItems] = useState([
+    { id: 1, label: " HTML & CSS ", checked: true },
+    { id: 2, label: " JavaScript ", checked: true },
+    { id: 3, label: " React JS ", checked: false },
+  ]);
+
+  return (
+    <main>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.id} className="item">
+              <input type="checkBox" checked={item.checked} />
+              <label>{item.label}</label>
+              <FaTrashCan role="button" tabIndex={0} />
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default Content;
