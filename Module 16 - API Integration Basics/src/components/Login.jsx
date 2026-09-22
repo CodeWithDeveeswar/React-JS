188ee12f9f60c68d29f82be02530f62d390cffbc
CodeWithDeveeswar <k.deveeswar@gmail.com>
2026-09-22 13:53:02 +0530

    Module 16 added

---
+++ Module 16 - API Integration Basics/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import { useNavigate, useParams } from "react-router-dom";
+
+const Login = () => {
+  let { newUser } = useParams();
+
+  let navigate = useNavigate();
+
+  let handleNavigate = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      Login - {newUser}
+      <button onClick={handleNavigate}>Move to Home</button>
+    </div>
+  );
+};
+
+export default Login;
