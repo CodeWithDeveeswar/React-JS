6266c996a85ce5df419826d23d8db3413531220a
CodeWithDeveeswar <k.deveeswar@gmail.com>
2026-09-22 12:02:16 +0530

    Module 15 added

---
+++ Module 15 - JSON Server/src/App.jsx
@@ -0,0 +1,33 @@
+import Home from "./components/Home";
+import Login from "./components/Login";
+import ProductDetails from "./components/ProductDetails";
+import ProductList from "./components/ProductList";
+import Products from "./components/Products";
+import SignUp from "./components/SignUp";
+import TodoApp from "./components/TodoApp";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />}>
+            <Route index element={<ProductList />} />
+            <Route path="list" element={<ProductList />} />
+            <Route path="details" element={<ProductDetails />} />
+          </Route>
+          <Route path="/login/:newUser" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/todo" element={<TodoApp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
